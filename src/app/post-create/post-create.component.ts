f5d651e4ff0a14b79cbbd8e4983c0b99125fa81d
Kunal Kumar <kmrknl2604@gmail.com>
2020-07-13 01:36:55 +0530

    Post model created

--- src/app/post-create/post-create.component.ts
+++ src/app/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../interfaces/post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -8,10 +9,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent {
   enteredTitle: string = '';
   enteredContent: string = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
   onAddPost = () => {
-    const POST = {title: this.enteredTitle, content: this.enteredContent}
+    const POST: Post = {title: this.enteredTitle, content: this.enteredContent}
     this.postCreated.emit(POST);
   }
 }
