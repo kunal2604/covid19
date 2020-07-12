c6fe723ffe3a27343b8d77c81cc261e17fbff3b4
Kunal Kumar <kmrknl2604@gmail.com>
2020-07-13 03:00:14 +0530

    Added Subject, Subscription in service

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
 import { Post } from '../interfaces/post.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
-  getPosts = () => {
-    // return [...this.posts]; // to prevent original array from modifying
-    return this.posts;
-  }
+  getPosts = () => [...this.posts];
+
+  getPostUpdateListener = () => this.postsUpdated.asObservable();
 
   addPost = (title: string, content: string) => {
     const POST: Post = {title: title, content: content};
     this.posts.push(POST);
+    this.postsUpdated.next([...this.posts]);
   }
 }
