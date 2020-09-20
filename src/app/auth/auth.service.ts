5d7cc0bcbc3477bed0617191aa22bfbaf28b7f32
Kunal Kumar <kmrknl2604@gmail.com>
2020-09-21 04:21:29 +0530

    Signup functionality working

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { URLS } from '../urls';
+import { AuthData } from './auth-data.model';
+
+@Injectable({ providedIn: "root" })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  createUser(email: string, password: string) {
+    const URL = URLS.CREATE_USER;
+    const AUTH_DATA: AuthData = { email: email, password: password };
+    this.http.post(URL, AUTH_DATA)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+}
